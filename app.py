e80017cbbb16acf0a8b8f1ffe8c546d05daff957
mcolave <mcolave@gmail.com>
2026-02-21 22:40:33 +0800

    Update app.run to bind to 0.0.0.0 and process PORT env var

--- app.py
+++ app.py
@@ -449,4 +449,5 @@ def debug_search():
     return jsonify({'query': query, 'result': result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
